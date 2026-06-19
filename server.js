8cb4004d0a8fafeca142cd3a7a3eba853928986e
Tushar Modi <moditushar100@gmail.com>
2026-06-19 17:00:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,8 @@
+//Server ko start krna
+
+const app = require("./src/app")
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
+
---
+++ src/app.js
@@ -0,0 +1,30 @@
+//Server ko create krna 
+
+const express = require("express");
+
+const app = express()
+app.use(express.json())
+
+
+const notes = []
+
+app.post('/notes', (req,res) => {
+    notes.push(req.body)
+
+    res.status(201).json({
+        message: "note created successfully"
+    })
+})
+
+
+app.get('/notes', (req, res) => {
+    res.status(200).json ({
+        message: "notes fetched successfully",
+        notes: notes
+    })
+})
+
+
+
+
+module.exports = app
