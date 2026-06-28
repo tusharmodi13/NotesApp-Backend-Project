f95f409ebd3bbc1f8858bde3cd1b08a4471eaf10
tusharmodi13 <moditushar100@gmail.com>
2026-06-28 12:40:42 +0530

    Again Re-Build the files

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 //Server ko start krna
+const app = require("./src/app");
 
-const app = require("./src/app")
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(3000,()=> {
+    console.log("Server is running on port 3000") 
 })
 
--- src/app.js
+++ src/app.js
@@ -1,34 +1,7 @@
-//Server ko create krna 
+// app.js file ka kaam hota h server ko create krna
 
-const express = require("express");
+const express = require('express');
 
-const app = express()
-app.use(express.json())
-
-
-const notes = []
-
-app.post('/notes', (req,res) => {
-    notes.push(req.body)
-
-    res.status(201).json({
-        message: "note created successfully"
-    })
-})
-
-
-app.get('/notes', (req, res) => {
-    res.status(200).json ({
-        message: "notes fetched successfully",
-        notes: notes
-    })
-})
-
-/* delete /notes/:index */
-
-app.delete('notes/index', (req, res) => {
-    
-})
-
+const app = express();
 
 module.exports = app
